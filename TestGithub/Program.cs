6a8692b8a437b4d4b7305b57069f572ff93629fb
lkh1945 <lkh_1945@duam.net>
2022-05-15 17:42:51 +0900

    리스트 변환

--- TestGithub/Program.cs
+++ TestGithub/Program.cs
@@ -10,10 +10,16 @@ namespace ConsoleApp2
         {
             //배열에서 3보다 큰 짝수를 찾는 함수 만들기
             // 가장 작은 짝수를 리턴한다.
-            List<int> testArray = new List<int>{ 1, 56, 2, 3, 4, 6 };
-            int returnNumber = testArray.Where(x => x > 3 && x % 2 == 0)
-                .OrderBy(x => x).First();
+            List<int> testList = new List<int>{ 1, 56, 2, 3, 4, 6 };
+            int returnNumber = testList.Where(x => x > 3 && x % 2 == 0)
+                .OrderBy(x => x)
+                .First();
             Console.WriteLine(returnNumber);
+
+            // && => and 조건 : true true 일 때 참
+            // a && b : a, b랑 둘 다 참일 때 참 반환
+            // || => or 조건 : 둘 중 하나라도 true이면 참 반환
+
         }
     }
 }
