fb39a74ffaf7a1c5e6a9b24b6565948fadb4c150
lkh1945 <lkh_1945@duam.net>
2022-05-15 17:20:45 +0900

    테스트 4

--- TestGithub/Program.cs
+++ TestGithub/Program.cs
@@ -1,30 +1,29 @@
 ﻿using System;
 
-namespace TestGithub
+namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             //배열에서 3보다 큰 짝수를 찾는 함수 만들기
-            int[] testArray = new int[] { 1, 2, 3, 4, 56, 6 };
+            int[] testArray = new int[] { 1, 56, 2, 3, 4, 6 };
             int returnNumber = FindNumber(testArray);
             Console.WriteLine(returnNumber);
-            
         }
 
         private static int FindNumber(int[] testArray)
         {
-            for(int i = 0; i < testArray.Length; i++)
+            for (int i = 0; i < testArray.Length; i++)
             {
                 int number = testArray[i];
-                int[] remain1 = new int[testArray.Length];
                 int remain = number % 2;
-                if(remain == 0)
+                if (remain == 0)
                 {
-                    number = remain1[i];
+                    return number;
                 }
             }
+
             return -1;
         }
     }
