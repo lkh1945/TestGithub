a27e70c50f31221f5589b19557b9f0bdf099abdb
Study <Study@DESKTOP-NH8UC2U>
2022-05-15 16:11:29 +0900

    초기 커밋.

---
+++ TestGithub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGithub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
